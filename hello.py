0e297992fd98a7190bf256a90b90d939c0cc34bd
David Wise <dwise@H-P-EL1A-MNG01.hentsu.corp>
2017-09-08 20:21:42 +0000

    Added health check, import of jsonify

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+import psutil
 
 app = Flask(__name__)
 
@@ -8,7 +9,14 @@ def hello_name(name):
 
 @app.route('/health')
 def health():
-    return 'Metrics:'
+    p = psutil.Process()
+    json_body = {
+        "cpu_percent": p.cpu_percent(interval=None),
+        "cpu_times": p.cpu_times(),
+        "mem_info": p.memory_info(),
+        "mem_percent": p.memory_percent()
+    }
+    return jsonify(json_body), 200
 
 @app.route('/counts')
 def counts():
