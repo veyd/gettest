c5c3c638e07fe12a351e2a8df43a9ee895890e1e
David Wise <dwise@H-P-EL1A-MNG01.hentsu.corp>
2017-09-08 20:29:55 +0000

    Some refactoring + added error 404 handler

--- hello.py
+++ hello.py
@@ -44,6 +44,9 @@ def reset():
     db.session.commit()
     return 'Count reset.'
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
-
