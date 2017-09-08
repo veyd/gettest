7a2d91b0f3f63119935ba63c89ba199061494978
David Wise <dwise@H-P-EL1A-MNG01.hentsu.corp>
2017-09-08 20:27:54 +0000

    Added methods, fleshed out counts, added db/sqlalchemy, added db entries on visit to hello page

--- hello.py
+++ hello.py
@@ -1,13 +1,27 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import psutil
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
-@app.route('/hello/:<name>')
+
+class Visit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    count = db.Column(db.Integer)
+
+    def __init__(self):
+        self.count = 0
+
+@app.route('/hello/:<name>', methods=['GET'])
 def hello_name(name):
+    v = Visit.query.first()
+    v.count +=1
+    db.session.commit()
     return 'Hello, ' + name + '!'
 
-@app.route('/health')
+@app.route("/health", methods=['GET'])
 def health():
     p = psutil.Process()
     json_body = {
@@ -18,6 +32,18 @@ def health():
     }
     return jsonify(json_body), 200
 
-@app.route('/counts')
+@app.route('/counts', methods=['GET'])
 def counts():
-    return 'Count!'
+    v = Visit.query.first()
+    return jsonify(counter=v.count)
+
+@app.route('/counts', methods=['DELETE'])
+def reset():
+    v = Visit.query.first()
+    v.count = 0
+    db.session.commit()
+    return 'Count reset.'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
